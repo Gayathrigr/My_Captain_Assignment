897e1b1d8ea60254366526cc4525ce7cf830fcbe
Gayathrigr <103306115+Gayathrigr@users.noreply.github.com>
2022-05-08 20:45:49 +0530

    Add files via upload

---
+++ most_frequent.py
@@ -0,0 +1,11 @@
+#Write Python code to create a function called most_frequent that takes a string and prints the letters in decreasing order of frequency. Use dictionaries.
+
+def most_frequent(s):
+    d = dict()
+    for key in s:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+print (most_frequent('Mississippi'))
